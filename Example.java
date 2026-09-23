b690b150ba4ff1881ca5d1929090caf07e682957
venuja200608 <jayasuriyavenuja2006.com>
2026-09-23 11:07:38 +0530

    Commit 4

--- Example.java
+++ Example.java
@@ -2,5 +2,6 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Welcome to the site");
+        System.out.println("This is a simple Java program");
     }
 }
